caf5d787866ab69de34c1ec3df64385136bef069
porirain1 <porirain@gmail.com>
2023-01-20 10:41:52 +0900

    [이지현] ajax 공통 함수 추가

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -63,3 +63,84 @@ var commaCellsRenderer = function(row, column, value) {
 	}
 }
 
+function goAjaxGetWithoutLoader(url, param, callback) {
+	var data = goAjaxMethod('GET', url, param, callback != null, callback, false); //비동기 사용
+	return data;
+}
+
+function goAjaxPostWithoutLoader(url, param, callback) {
+	var data = goAjaxMethod('POST', url, param, callback != null, callback, false); //비동기 사용
+	return data;
+}
+
+function goAjaxGet(url, param, callback) {
+	var data = goAjaxMethod('GET', url, param, callback != null, callback, true); //비동기 사용
+	return data;
+}
+
+function goAjaxPost(url, param, callback) {
+	var data = goAjaxMethod('POST', url, param, callback != null, callback, true); //비동기 사용
+	return data;
+}
+
+function goAjaxMethod(method, url, param, async, callback, useLoader) {
+	var response 		= null;
+	var ajaxOption 		= {};
+	ajaxOption.url 		= url;
+	ajaxOption.type 	= method;
+	ajaxOption.dataType = 'json';
+	ajaxOption.async 	= async;
+	ajaxOption.beforeSend = function() {
+		// start progress 
+		if ( useLoader == true ) {
+			$('#jqxLoader').jqxLoader('open');
+		}
+	} 
+	ajaxOption.complete = function() {
+		// end progress 
+		if ( useLoader == true ) {
+			$('#jqxLoader').jqxLoader('close');
+		}
+	}
+	
+	if ( method == 'GET' ) {
+		if ( param != null ) {
+			if ( param instanceof Object ) {
+				param = objectToQueryString(param);
+			}
+			ajaxOption.data = param;	
+		}
+	}
+	else {
+		ajaxOption.contentType 	= 'application/json; charset=UTF-8';
+		if ( param != null ) {
+			ajaxOption.data = JSON.stringify(param);	
+		}	
+	}
+	
+	if (callback != null) {
+		$.ajax(ajaxOption).done(callback).fail(function(data) {
+			if ( data.responseText != null && data.responseText.indexOf('loginForm') > 0 ) {
+				window.location.href = '/logout';
+			}
+		});
+	}
+	else {
+		$.ajax(ajaxOption).done(function(data) {
+			response = data;
+		}).fail(function(data) {
+			if ( data != null && xhr.data.indexOf('loginForm') > 0 ) {
+				window.location.href = '/logout';
+			}
+		});	
+
+		return response;
+	}
+}
+
+function objectToQueryString(json) {
+    return Object.keys(json).map(function(key) {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(json[key]);
+    }).join('&');
+}
+
